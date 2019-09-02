6867e20afc40642163c4db436a9d162ab8e6c62f
TSentner <centner_dc@mail.ru>
2019-09-02 21:52:42 +0500

    solution to task 1

--- Lesson1/Task1Users/Program.cs
+++ Lesson1/Task1Users/Program.cs
@@ -10,6 +10,60 @@ namespace Task1Users
     {
         static void Main(string[] args)
         {
+            var DBUsers = new SystemUsers(new User[]
+            {
+                new User(0, "Сергей", 100),
+                new User(1, "Егор", 200),
+                new User(2, "Александр", 10)
+            });
         }
     }
+
+    class User
+    {
+        public int Id;
+        public string Name;
+        public float Salary;
+
+        public User(int id, string name, float salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+    }
+
+    class SystemUsers
+    {
+        private User[] Users;
+
+        public SystemUsers(User[] users)
+        {
+            Users = users;
+        }
+
+        public User GetById(int id)
+        {
+            foreach (var item in Users)
+                if (item.Id == id)
+                    return item;
+
+            return null;
+        }
+
+        public User FirstByName(string name)
+        {
+            foreach (var item in Users)
+                if (item.Name == name)
+                    return item;
+
+            return null;
+        }
+
+        public User[] SalaryGreaterThan(float comparable) => Users.Where(user => user.Salary > comparable).ToArray();
+        public User[] SalaryLessThan(float comparable) => Users.Where(user => user.Salary < comparable).ToArray();
+        public User[] SalaryBetween(float min, float max) => Users.Where(user => min <= user.Salary && user.Salary <= max).ToArray();
+
+    }
+       
 }
