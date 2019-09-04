4e8942347b7f2d1e1fc73e6d3f2ddd478f7440db
TSentner <centner_dc@mail.ru>
2019-09-04 21:07:26 +0500

    Сorrection of mistakes

--- Lesson1/Task1Users/Program.cs
+++ Lesson1/Task1Users/Program.cs
@@ -10,20 +10,20 @@ namespace Task1Users
     {
         static void Main(string[] args)
         {
-            var DBUsers = new SystemUsers(new User[]
+            var DBUsers = new UsersManager(new User[]
             {
                 new User(0, "Сергей", 100),
                 new User(1, "Егор", 200),
-                new User(1, "Александр", 10)
+                new User(2, "Александр", 10)
             });
         }
     }
 
     class User
     {
-        public int Id;
-        public string Name;
-        public float Salary;
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public float Salary { get; private set; }
 
         public User(int id, string name, float salary)
         {
@@ -33,42 +33,40 @@ namespace Task1Users
         }
     }
 
-    class SystemUsers
+    class UsersManager
     {
-        private User[] Users;
+        private User[] _users;
 
-        public SystemUsers(User[] users)
+        public UsersManager(User[] users)
         {
             if (CheckUniqueId(users))
-                Users = users;
+                _users = users;
             else
                 throw new Exception("When creating SystemUsers, a non-unique user was found");
-            
         }
 
-        private bool CheckUniqueId(User[] users) 
-            => !users.GroupBy(user => user.Id)
-                     .Select(group => new { Count = group.Count()})
-                     .Any(group => group.Count > 1);
-
-        public User GetById(int id)
+        private bool CheckUniqueId(User[] users)
         {
-            var query = Filter(user => user.Id == id);
-            return query.Length > 0 ? query[0] : null;
+            var allId = (from user in users
+                         orderby user.Id
+                         select user.Id).ToArray();
+            for (int i = 1; i < allId.Length; i++)
+                if (allId[i - 1] == allId[i])
+                    return false;
+
+            return true;
         }
 
-        public User FirstByName(string name)
-        {
-            var query = Filter(user => user.Name == name);
-            return query.Length > 0 ? query[0] : null;
-        }
+
+        public User GetById(int id) => _users.FirstOrDefault(user => user.Id == id);
+        public User FirstByName(string name) => _users.FirstOrDefault(user => user.Name == name);
 
         public User[] SalaryGreaterThan(float comparable) => Filter(user => user.Salary > comparable);
         public User[] SalaryLessThan(float comparable) => Filter(user => user.Salary < comparable);
         public User[] SalaryBetween(float min, float max) => Filter(user => min <= user.Salary && user.Salary <= max);
 
 
-        private User[] Filter(Predicate<User> query) => Users.Where(user => query(user)).ToArray();
+        private User[] Filter(Predicate<User> query) => _users.Where(user => query(user)).ToArray();
     }
        
 }
