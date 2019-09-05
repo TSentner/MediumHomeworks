782bf07d4b3c82a62cc34edaf92625b47ef900d3
TSentner <centner_dc@mail.ru>
2019-09-05 20:51:53 +0500

    Task4 complete

--- Lesson1/Task1Users/Program.cs
+++ Lesson1/Task1Users/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task1Users
 {
@@ -10,6 +6,55 @@ namespace Task1Users
     {
         static void Main(string[] args)
         {
+        }
+    }
+
+    class Player 
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+
+        public Movement Movement { get; private set; } = new Movement();
+        public Weapon Weapon { get; private set; } = new Weapon();
+
+        public Player(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void Attack()
+        {
+            //attack
+        }
+    }
+
+    class Weapon
+    {
+        public int Damage { get; private set; }
+        public float Cooldown { get; private set; }
+        public bool IsReloading()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Movement
+    {
+        public float DirectionX { get; private set; }
+        public float DirectionY { get; private set; }
+        public float Speed { get; private set; }
+
+        public void SetDirection(float x, float y)
+        {
+            DirectionX = x;
+            DirectionY = y;
+        }
+        public void SetSpeed(float speed) => Speed = speed;
+
+        public void Move()
+        {
+            //Do move
         }
     }
 }
