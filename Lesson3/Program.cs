618542bc56cc8997bbbff67343ea06f1be98030b
TSentner <centner_dc@mail.ru>
2019-09-25 20:54:08 +0500

    Lesson 3 Task 2 complete

--- Lesson3/Program.cs
+++ Lesson3/Program.cs
@@ -11,112 +11,56 @@ namespace Lesson3
     {
         static void Main(string[] args)
         {
-            var goods = new Goods(new [] {
-                new Good("beer keg", 10, 5),
-                new Good("A", 4, 4),
-                new Good("B", 1, 3),
-                new Good("C", 2, 2),
-                new Good("D", 12, 1)
-            });
+            var products = new List<Product> {
+                new Product(1, "beer keg", 10, 6),
+                new Product(2, "A", 4, 4),
+                new Product(3, "B", 1, 3),
+                new Product(4, "C", 2, 2),
+                new Product(5, "D", 112, 9)
+            };
 
-            Console.WriteLine("\n" + goods.View() + "\n\n");
+            var query = products.Where(product => product.Cost < 100 && product.Count > 5);
 
-            goods.Sort(good => good.Name);
-            Console.WriteLine("Simple sort by Name: \n" + goods.View() + "\n\n");
-
-            goods.Sort(SortingGoods.By.Level);
-            Console.WriteLine("Sort by Level: \n" + goods.View() + "\n\n");
-
+            Console.WriteLine(query.Count());
 
             Console.ReadKey();
         }
     }
 
-    interface ISortable<T>
+    
+
+    public class Product
     {
-        IEnumerable<T> Sort(IEnumerable<T> enumerable);
-    }
-
-    public class SortingGoods<T> : ISortable<Good>
-    {
-        public Func<Good, T> SortFunc { get; private set; }
-
-        public SortingGoods(Func<Good, T> sort)
-        {
-            SortFunc = sort;
-        }
-
-        public IEnumerable<Good> Sort(IEnumerable<Good> enumerable)
-        {
-            return enumerable.OrderBy(SortFunc);
-        }
-    }
-
-    public class SortingGoods
-    {
-        public enum By
-        {
-            Name,
-            Cost,
-            Level
-        }
-
-        private Dictionary<By, ISortable<Good>> _sorts = new Dictionary<By, ISortable<Good>> {
-            { By.Name, new SortingGoods<string>(good => good.Name) },
-            { By.Cost, new SortingGoods<float>(good => good.Cost) },
-            { By.Level, new SortingGoods<int>(good => good.Level) },
-        };
-
-        public IEnumerable<Good> Sort(IEnumerable<Good> goods, By sortBy) => _sorts[sortBy].Sort(goods);
-    }
-       
-
-    public class Goods
-    {
-        private IEnumerable<Good> _goods;
-        private SortingGoods _goodSorts = new SortingGoods();
-
-        public Goods(IEnumerable<Good> goods)
-        {
-            _goods = goods;
-        }
-        
-        //Generic Simple Sort
-        public void Sort<T>(Func<Good, T> sort) => _goods = _goods.OrderBy(sort);
-
-        public void Sort(SortingGoods.By sortBy) => _goods = _goodSorts.Sort(_goods, sortBy);
-
-        public string View() =>
-            string.Concat(_goods.Select((good, i) => $"{i} {good.Name} {good.Cost} {good.Level} \n"));
-    }
-
-    public class Good
-    {
+        public readonly int Id;
         public string Name { get; private set; }
         public float Cost { get; private set; }
-        public int Level { get; private set; }
+        public int Count { get; private set; }
 
-        public Good(string name, float cost, int level)
+        public Product(int id, string name, float cost, int count)
         {
+            Id = id;
             Name = name;
             SetCost(cost);
-            SetLevel(level);
+            SetCount(count);
+        }
+
+        private void CheckPositive(float value, string error)
+        {
+            if (value < 0f)
+                throw new ArgumentOutOfRangeException(error);
         }
 
         private void SetCost(float cost)
         {
-            if (cost < 0f)
-                throw new ArgumentOutOfRangeException("cost");
-
+            CheckPositive(cost, "cost");
             Cost = cost;
         }
 
-        private void SetLevel(int level)
+        private void SetCount(int count)
         {
-            if (level < 0f)
-                throw new ArgumentOutOfRangeException("level");
-
-            Level = level;
+            CheckPositive(count, "count");
+            Count = count;
         }
+
     }
 }
